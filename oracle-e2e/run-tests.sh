511a9cf57dc4dd0a76b6a4a71ab3a41acf624f61
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2020-01-10 14:37:42 -0300

    fix wait for validators in oracle e2e

--- oracle-e2e/run-tests.sh
+++ oracle-e2e/run-tests.sh
@@ -3,7 +3,7 @@ cd $(dirname $0)
 ../e2e-commons/up.sh deploy oracle oracle-validator-2 oracle-validator-3
 
 # wait for validators
-sleep 10
+sleep 60
 
 docker-compose -f ../e2e-commons/docker-compose.yml run e2e yarn workspace oracle-e2e run start
 rc=$?
